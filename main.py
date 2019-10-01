4e16525504f9e38191da558ba5230f4e455de04d
Tanner <tdogbeard@gmail.com>
2019-10-01 15:08:42 -0400

    update

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
 import pyzed.sl as sl
+import rospy
+import std_msgs.msg
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
 zed = sl.Camera()
 init = sl.InitParameters()
@@ -18,24 +22,33 @@ image_ocv = image_zed.get_data()
 runtime = sl.RuntimeParameters()
 runtime.sensing_mode = sl.SENSING_MODE.SENSING_MODE_STANDARD
 
+image_pub = rospy.Publisher('image_publisher_zed', Image, queue_size=10)
+bridge = CvBridge()
+
 def grabFrames():
     if zed.grab(runtime) == sl.ERROR_CODE.SUCCESS :
         zed.retrieve_image(image_zed, sl.VIEW.VIEW_LEFT)
         image_ocv = image_zed.get_data() #numpy array
-        cv2.imshow("Image", image_ocv)
+        ros_img = bridge.cv2_to_imgmsg(image_ocv, "8UC4")
+        image_pub.publish(ros_img)
 
 def initProcessing(plane, transform):
     print("Init spatial mapping and camera pose")
-    zed.enable_spatial_mapping()
-    zed.enable_tracking()
-    zed.find_floor_plane(plane, transform)
+    spatial_parameters = sl.SpatialMappingParameters()
+    zed.enable_spatial_mapping(spatial_parameters)
+    tracking_parameters = sl.TrackingParameters(transform)
+    zed.enable_tracking(tracking_parameters)
+    #zed.find_floor_plane(plane, transform)
 
 def main():
+    rospy.init_node("rosnode_zed")
     signalRecieved = True
     while not signalRecieved:
-    
+        pass
     plane = sl.Plane()
-    transform = sl.Transform()
+    data = np.array([0,0,0,0])
+    trasnform_matrix = sl.Matrix4f(data)
+    transform = sl.Transform(trasnform_matrix)
     initProcessing(plane, transform)
 
     while True:
